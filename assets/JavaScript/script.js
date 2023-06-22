43f020d01e302de6d560fa62407c2b5aac1ffec5
GomesDevW <118786623+GomesDevW@users.noreply.github.com>
2023-06-22 02:15:33 -0300

    Update script.js
    add a js code

---
+++ assets/JavaScript/script.js
@@ -0,0 +1,50 @@
+const test = document.querySelector("#button")
+const test2 = document.querySelector("#message-button")
+const container = document.querySelector('.container')
+const message = document.querySelector('.message-container')
+const email = document.querySelector('#user-email')
+const input = document.querySelector('#email')
+const error = document.querySelector('.email-field span')
+
+
+
+const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+function subscribe(){
+
+    if(input.value === "" ){      
+        
+        error.style.display = 'block'
+        input.style.backgroundColor = 'rgb(250, 218, 218)'       
+        input.style.border = '1px solid rgb(255, 0, 0)'
+            
+    } else if(!input.value.match(mailformat)){
+        
+        error.style.display = 'block'
+        input.style.backgroundColor = 'rgb(250, 218, 218)'
+        input.style.border = '1px solid rgb(255, 0, 0)'
+        
+    }else{
+       
+        email.innerText = input.value
+        container.style.display = 'none'
+        message.style.display = 'flex'
+    }
+    
+
+
+}
+
+function demiss(){
+ 
+    container.style.display = 'flex'
+    message.style.display = 'none'
+    error.style.display = 'none'
+    input.style.backgroundColor = '#fff'
+    input.style.border = '1px solid #ccc'
+    input.value = ""
+}
+
+test.addEventListener('click', subscribe)
+test2.addEventListener('click', demiss)
